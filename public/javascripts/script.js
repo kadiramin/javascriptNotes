48a6626fad14413c18db0b774fa3f244f9fc07c1
kadiramin <kadiramin@msn.com>
2022-06-03 22:38:23 +0100

    modifid  script.js

---
+++ public/javascripts/script.js
@@ -0,0 +1,17 @@
+const header = document.createElement('h1');
+const msg=('Yay, Our SandBox Works!');
+document.querySelector('h1').innerHTML=msg;
+
+
+
+
+const theTest='testing functions';
+
+function newTest () {
+  // cb = 'no testing today!'
+  return console.info(theTest)
+  
+
+  
+}
+newTest();
